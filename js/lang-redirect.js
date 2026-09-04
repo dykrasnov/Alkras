08e85b9889701948981930a686621c27772f7cb9
Daniel Krasnov <dykrasnov@gmail.com>
2026-09-04 06:07:32 +0000

    Update homepage path check for new repo name (Alkras)

--- js/lang-redirect.js
+++ js/lang-redirect.js
@@ -1,7 +1,7 @@
 (function() {
   // Only run on the homepage
   if (window.location.pathname.split('/').filter(Boolean).pop() !== 'index.html' &&
-      !window.location.pathname.endsWith('/alkras-site/') &&
+      !window.location.pathname.endsWith('/Alkras/') &&
       !window.location.pathname.endsWith('/')) return;
   var pref = localStorage.getItem('alkras-lang');
   if (pref === 'ru' && window.location.pathname.indexOf('/ru/') === -1) {
